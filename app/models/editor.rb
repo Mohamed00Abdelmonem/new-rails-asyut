59027ffb9af64ba912314415f531aeef92a03ab1
Mohamed00Abdelmonem <mmohamedabdelm@gmail.com>
2026-06-14 15:06:37 +0300

    add: Join Table and add Many-to-Many

--- app/models/editor.rb
+++ app/models/editor.rb
@@ -1,2 +1,4 @@
 class Editor < ApplicationRecord
+  has_many :editor_posts, dependent: :destroy
+  has_many :posts, through: :editor_posts
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :creator,
              class_name: "User"
+
+  has_many :editor_posts, dependent: :destroy
+  has_many :editors, through: :editor_posts
 end
