7b3eed2f5d379e4c628d724350f4f8b6df69da69
Mohamed00Abdelmonem <mmohamedabdelm@gmail.com>
2026-06-14 08:32:14 +0300

    add : CRUD for posts and migratinos for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,51 @@
 class PostsController < ApplicationController
-  # your code goes here
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  # GET /posts
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/:id
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(title: params[:title], content: params[:content])
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # GET /posts/:id/edit
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  # PATCH/PUT /posts/:id
+  def update
+    if @post.update(title: params[:title], content: params[:content])
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /posts/:id
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+
 end
